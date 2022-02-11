381b2411598a0c981b46ce9ca9090061c8d0e1b9
thitimon171143 <montha.kam.iam@gmail.com>
2022-02-11 15:56:12 +0700

    hw2

---
+++ android/app/src/main/kotlin/com/example/hw2_appdesign/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hw2_appdesign
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'register.dart';
+import 'dart:ui';
+
+void main() {
+  var app = runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My App",
+      home: MyHomeApp(),
+      theme: ThemeData(primarySwatch: Colors.pink),
+    );
+  }
+}
+
+class MyHomeApp extends StatefulWidget {
+  const MyHomeApp({Key? key}) : super(key: key);
+
+  @override
+  _MyHomeAppState createState() => _MyHomeAppState();
+}
+
+class _MyHomeAppState extends State<MyHomeApp> {
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> data = [];
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Register",
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(30.10),
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/images/person.jpg",
+                height: 200,
+              ),
+              Container(
+                padding: const EdgeInsets.all(20.30),
+              ),
+              Register("assets/images/img1", "Username", Colors.grey),
+              Container(
+                padding: const EdgeInsets.all(5.30),
+              ),
+              Register("assets/images/img1", "Email", Colors.grey),
+              Container(
+                padding: const EdgeInsets.all(20.30),
+                child: TextButton(
+                  child: Text(
+                    "Register",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  style: TextButton.styleFrom(
+                    primary: Colors.white,
+                    backgroundColor: Colors.pinkAccent,
+                  ),
+                  onPressed: () {},
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+}
